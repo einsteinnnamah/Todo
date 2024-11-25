a00e73757d77974eab8e658f71bda3771b1353ff
Einstein Nnamah <einsteinnnamah@Einsteins-MacBook-Pro.local>
2024-11-25 01:14:54 +0100

    added pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+})
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = withPWA(nextConfig)
